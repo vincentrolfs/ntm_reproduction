71e3fe3a143379804827977ef6345677248f5933
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-05-26 21:28:32 +0800

    Updated constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,6 @@
 SEED = 0
 
-OPTIMIZER_LOAD_PATH = '../optimizer_checkpoint_1590197499_29999/optimizer_checkpoint.pickle'
+OPTIMIZER_LOAD_PATH = 'optimizer_checkpoint_1590197499_29999/optimizer_checkpoint.pickle'
 MODEL_LOAD_PATH = 'model_checkpoint_1590197499_29999/model_checkpoint.ckpt'
 
 OPTIMIZER_SAVE_PATH_PREFIX = 'optimizer_checkpoint'
@@ -8,7 +8,7 @@ OPTIMIZER_SAVE_FILENAME = 'optimizer_checkpoint.pickle'
 MODEL_SAVE_PATH_PREFIX = 'model_checkpoint'
 MODEL_SAVE_FILENAME = 'model_checkpoint.ckpt'
 
-AMOUNT_BATCHES = 10  # How many batches should we use?
+AMOUNT_BATCHES = 30000  # How many batches should we use?
 BATCH_SIZE = 32  # How many sequences should one batch contain?
 MAX_SEQUENCE_LENGTH = 20  # How many vectors should a sequence contain at maximum?
 MIN_SEQUENCE_LENGTH = 2  # How many vectors should a sequence contain at minimum?
@@ -23,8 +23,8 @@ MOMENTUM = 0.9  # Only for RMSPROP
 DECAY = 0.9  # Only for RMSPROP
 MAX_GLOBAL_GRAD_NORM = 50
 
-TRAINING_PROGRESS_DISPLAY_INTERVAL = 5  # In seconds
-PROGRESS_SAVE_INTERVAL = 1  # In train steps
+TRAINING_PROGRESS_DISPLAY_INTERVAL = 60  # In seconds
+PROGRESS_SAVE_INTERVAL = 3000  # In train steps
 
 ERROR_VISUALIZATION_FILENAME = 'errors'
 ERROR_VISUALIZATION_FILE_EXTENSION = '.pdf'
