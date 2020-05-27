ea812a5d3602331aee459cfa08e67c3ab58d9cd1
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-05-27 16:28:03 +0800

    don't save error visualization during training

--- train.py
+++ train.py
@@ -41,4 +41,3 @@ for batch_index in range(AMOUNT_BATCHES):
         file_saver.save_model(model, batch_index)
         file_saver.save_optimizer(optimizer, batch_index)
         file_saver.save_losses(losses, batch_index)
-        monitor.save_error_visualization(batch_index, labels, outputs, sequence_length)
