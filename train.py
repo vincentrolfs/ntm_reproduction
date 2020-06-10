2fe636225ccf467a2df1d82e5a2d942bbb672493
Vincent Rolfs <vincent.rolfs@gmail.com>
2020-06-10 16:12:21 +0800

    Moved model to training directory

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
-from Model import Model
 from config.config_loader import MAX_GLOBAL_GRAD_NORM, AMOUNT_BATCHES, PROGRESS_SAVE_INTERVAL, VALIDATION_INTERVAL
 from data_factory import get_batch
+from training.Model import Model
 from training.loss_function import get_loss_function
 from training.optimizer import get_optimizer
 from training.validate import validate
